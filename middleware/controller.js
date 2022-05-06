dd6b95f720d18f56b64dd2fe8ae5a51906e97284
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-05-07 01:49:36 +0530

    Create new request to get the snake from specs

--- middleware/controller.js
+++ middleware/controller.js
@@ -1,48 +1,115 @@
-const res = require('express/lib/response')
-const mongoose = require('mongoose')
-const Snake = require('./model')
+const mongoose = require("mongoose");
+const Snake = require("./model");
 
 exports.getSnakes = (req, res, next) => {
-    console.log("Getting all snakes from database") // TODO : Remove
-    Snake.find()
-    .select("_id color description firstAid headShape image name otherName pattern scientificName venomLevel")
+  console.log("Getting all snakes from database"); // TODO : Remove
+  Snake.find()
+    .select(
+      "_id color description firstAid headShape image name otherName pattern scientificName venomLevel"
+    )
     .exec()
-    .then(data => {
-        res.status(200).json({
-            snakes: data
-        })
+    .then((data) => {
+      res.status(200).json({
+        snakes: data,
+      });
     })
-    .catch(err => {
-        res.status(500).json({
-            error: "unable to fetch data from database"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).json({
+        error: "unable to fetch data from database",
+      });
+    });
+};
 
 exports.createSnake = (req, res, next) => {
-    const newSnake = new Snake({
-        _id: new mongoose.Types.ObjectId,
-        color: req.body.color,
-        description: req.body.description,
-        firstAid: req.body.firstAid,
-        headShape: req.body.headShape,
-        image: req.body.image,
-        name: req.body.name,
-        otherName: req.body.otherName,
-        pattern: req.body.pattern,
-        scientificName: req.body.scientificName,
-        venomLevel: req.body.venomLevel,
-    })
-    newSnake
+  const newSnake = new Snake({
+    _id: new mongoose.Types.ObjectId(),
+    color: req.body.color,
+    description: req.body.description,
+    firstAid: req.body.firstAid,
+    headShape: req.body.headShape,
+    image: req.body.image,
+    name: req.body.name,
+    otherName: req.body.otherName,
+    pattern: req.body.pattern,
+    scientificName: req.body.scientificName,
+    venomLevel: req.body.venomLevel,
+  });
+  newSnake
     .save()
-    .then(data => {
-        console.log(data)
-        res.status(201).json({
-            "message": "New Snake Created!",
-            "data" : newSnake._id
+    .then((data) => {
+      res.status(201).json({
+        message: "New Snake Created!",
+        data: newSnake._id,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getSnakeFromSpec = (req, res, next) => {
+  const param1 = req.body.pattern;
+  const param2 = req.body.headShape;
+  const param3 = req.body.color;
+
+  if (param1 === "" || param2 === "" || param3 === "") {
+    return res.status(400).json({
+      status: false,
+      message: "Params cannot be null",
+      data: null,
+    });
+  }
+
+  const snakeId = new Promise((resolve, reject) => {
+    Snake.find({ pattern: param1, headShape: param2 })
+      .select("_id color headShape pattern")
+      .exec()
+      .then((data) => {
+        data?.find((s) => {
+          const sColor = s["color"];
+          if (sColor !== "" || sColor !== undefined || sColor !== null) {
+            if (sColor.includes(param3)) {
+              console.log(s["_id"]?.valueOf());
+              resolve(s["_id"]?.valueOf());
+            }
+          }
+          reject("cannot found");
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        reject("cannot found");
+      });
+  });
+
+  snakeId
+    .then((id) => {
+      Snake.find({ _id: id })
+        .select(
+          "_id color description firstAid headShape image name otherName pattern scientificName venomLevel"
+        )
+        .exec()
+        .then((data) => {
+          console.log(data);
+          res.status(200).json({
+            status: true,
+            message: "Snake queried successfully",
+            data: data,
+          });
         })
+        .catch((err) => {
+          res.status(400).json({
+            status: false,
+            message: "Cannot find snake for that id",
+            data: null,
+          });
+        });
     })
-    .catch(err => {
-        console.log(err)
-    })
-}
+    .catch((err) => {
+      res.status(400).json({
+        status: false,
+        message: "Couldn't find any mating result",
+        data: null,
+      });
+    });
+};
